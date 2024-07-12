c8164b8de25196f1821c2ca23b508c3ff56f91f4
Yuxuan Xie <teststaybaka@gmail.com>
2024-07-12 13:46:06 -0700

    Added common send() override. Release

--- client_mock.ts
+++ client_mock.ts
@@ -1,7 +1,19 @@
 import { WebServiceClient } from "./client";
 
 export class WebServiceClientMock extends WebServiceClient {
+  public request: any; // captured
+  public error: Error; // to throw
+  public response: any; // to return;
+
   public constructor() {
     super(undefined, undefined, undefined);
+  }
+  public async send(request: any): Promise<any> {
+    this.request = request;
+    if (this.error) {
+      throw this.error;
+    } else {
+      return this.response;
+    }
   }
 }
